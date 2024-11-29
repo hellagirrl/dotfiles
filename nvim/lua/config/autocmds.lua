c73a20cd6dd1202642d7bb494bc2f8d29fed269a
kat <catherineitj28@gmail.com>
2024-11-29 06:28:09 +0300

    Remove nvim config as a submodule but keep it as a folder

---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,24 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+	pattern = "*",
+	command = "set nopaste",
+})
+
+-- Disable the concealing in some file formats
+-- The default conceallevel is 3 in LazyVim
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json", "jsonc", "markdown" },
+	callback = function()
+		vim.opt.conceallevel = 0
+	end,
+})
+
+vim.cmd([[
+    autocmd BufEnter * if &filetype != 'lazyvim' | setlocal colorcolumn=80,120 | endif
+]])
+vim.api.nvim_command("set wrap")
+vim.api.nvim_buf_set_option(0, "shiftwidth", 4)
